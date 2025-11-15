9a11496ce52d3ccd98d3292e92f9fa4cda05b3d9
Antonio Dev <jesustuncupul@gmail.com>
2025-11-15 17:43:57 -0500

    .

---
+++ playground/quotes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("<int:day>/", views.days_week_with_number,name="days_week_with_number"),
+    path("<str:day>/", views.days_week,name="day-quote"),
+]
